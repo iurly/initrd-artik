a1947887a4c8746d537df2457c10471cb2e719d5
Gerlando Falauto <gerlando.falauto@st.com>
2019-07-14 13:58:01 +0200

    recovery for rosamicro LEDS

--- board/artik710/sbin/post_recovery.sh
+++ board/artik710/sbin/post_recovery.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-LED_RED=28
-LED_BLUE=38
-GPIO_PATH=/sys/class/gpio
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 ZIGBEE_SCRIPT=802.15.4_setup.sh
 ZIGBEE_FW_NAME=NONE
 ZIGBEE_FW_TYPE=1
 ZIGBEE_FW_VERSION=5.7.4
 
-led_ctl()
-{
-	local led=$1
-	local ctl=$2
-
-	if ! [ -d $GPIO_PATH/gpio$led ]; then
-		echo $led > $GPIO_PATH/export
-	fi
-
-	echo "out" > $GPIO_PATH/gpio$led/direction
-	echo $ctl > $GPIO_PATH/gpio$led/value
-}
 
 wait_zb_process()
 {
@@ -29,8 +14,8 @@ wait_zb_process()
 	done
 }
 
-led_ctl $LED_RED 0
-led_ctl $LED_BLUE 1
+# Turn off red led
+echo none > /sys/class/leds/red/trigger
 
 # Zigbee firmware check
 
@@ -49,6 +34,8 @@ fi
 
 if [ $RET == 0 ]; then
 	echo "The current $ZIGBEE_FW_STR fw is the latest version"
+	# Make a solid green light
+	echo 100 > /sys/class/leds/green/brightness
 	exit 0
 else
 	echo "Invalid $ZIGBEE_FW_STR fw version"
--- board/artik710/sbin/pre_recovery.sh
+++ board/artik710/sbin/pre_recovery.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 
-LED_RED=28
-LED_BLUE=38
-GPIO_PATH=/sys/class/gpio
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 ZIGBEE_SCRIPT=802.15.4_setup.sh
 ZIGBEE_FW_NAME=ncp-uart-xon-xoff-use-with-serial-uart-btl-5.7.4.ebl
 ZIGBEE_FW_TYPE=1
 ZIGBEE_FW_VERSION=5.7.4
 
-led_ctl()
-{
-	local led=$1
-	local ctl=$2
-
-	if ! [ -d $GPIO_PATH/gpio$led ]; then
-		echo $led > $GPIO_PATH/export
-	fi
-
-	echo "out" > $GPIO_PATH/gpio$led/direction
-	echo $ctl > $GPIO_PATH/gpio$led/value
-}
-
-led_ctl $LED_RED 1
+# Make a red blinking during recovery
+echo timer > /sys/class/leds/red/trigger
 
 CUR_DIR=$(pwd)
 cd $SCRIPT_DIR
